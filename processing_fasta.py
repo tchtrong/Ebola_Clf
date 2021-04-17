1d6d383838a4118b79c4b7af96beca3c5a299d91
Tran Trong <tchtrong@apcs.vn>
2021-04-17 17:58:45 +0700

    First commit

---
+++ processing_fasta.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 17 02:15:57 2021
+
+@author: binn
+"""
+
+#%%
+from global_variables import organisms
+
+from Bio import SeqIO
+
+import random
+
+#%%
+records = {}
+for organism in organisms:
+    record = list(SeqIO.parse("fasta_original/{}.fasta".format(organism), "fasta"))
+    record = [sequence for sequence in record if str(sequence.seq).find("X") == -1]
+    if (organism == "zai"):
+        record = random.sample(record, 270)
+    records.update({organism: record})
+
+#%%
+all_records = []
+for organism in organisms:
+    all_records = all_records + list(records[organism])
+    SeqIO.write(records[organism], "fasta/{}.fasta".format(organism), "fasta")
+print(len(all_records))
+SeqIO.write(all_records, "motifs/all.fasta", "fasta")
