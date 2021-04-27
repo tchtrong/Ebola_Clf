6ea354871c567404aec6d41796a70fb3133f8e5c
Tran Trong <tchtrong@apcs.vn>
2021-04-27 20:39:30 +0700

    Preprocessing motifs and frequency matrices
    Former-commit-id: 10dd05e9b64eb84d56143db75fc721e6b764e27c [formerly 441258cbd1e52be6a66c6423b471c1e459abcb21]
    Former-commit-id: 0879d8d883f9ee70a3663322a6a0b247dae149af

---
+++ processing_motifs.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 18:45:04 2021
+
+@author: binn
+
+"""
+
+#%%
+#Read fasta files
+from Bio import SeqIO
+import xml.etree.ElementTree as ET
+import re
+import pandas as pd
+import numpy as np
+
+#%%
+#Records
+sequences = [str(sequence.seq) for sequence in list(SeqIO.parse("fasta/all.fasta", "fasta"))]
+
+#%%
+#Motifs
+motifs = []
+for idx in range(3, 21):
+    for child in ET.parse("motifs/{}_{}/meme.xml".format(idx, idx)).getroot()[2]:
+        motif = child[2].text.replace("\n", "")
+        motifs.append(motif)
+        
+#%%
+freq_matrix = []
+for sequence in sequences:
+    f_motifs_in_sequence = []
+    for motif in motifs:
+        f_motifs_in_sequence.append(len(re.findall(motif.replace("X", "[A-Z]"), sequence)))
+    freq_matrix.append(f_motifs_in_sequence)
+    
+#%%
+freq_matrix_csv = pd.DataFrame(freq_matrix, index=[sequence for sequence in sequences], columns=(motifs))
+
+#%%
+sum_of_freq_each_motif = list(freq_matrix_csv.sum().array)
+
+#%%
+freq_matrix_csv.drop(columns=([motif for idx, motif in enumerate(motifs) if sum_of_freq_each_motif[idx] == 0]), inplace=(True))
+
+#%%
+freq_matrix_csv.to_csv("csv/freq_matrix.csv")
+
+#%%
+freq_matrix_without_X = freq_matrix_csv.drop(columns=[motif for motif in list(freq_matrix_csv.columns.values) if motif.count("X") != 0])
+freq_matrix_without_X.to_csv("csv/freq_matrix_without_X.csv")
+
+#%%
+freq_matrix_with_X_less_than_half = freq_matrix_csv.drop(columns=[motif for motif in list(freq_matrix_csv.columns.values) if motif.count("X") >= round(len(motif) / 2)])
+freq_matrix_with_X_less_than_half.to_csv("csv/freq_matrix_with_X_less_than_half.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
