3599c0294a3ee4b188e7aa12be9bc2c7cca02cbb
Hvmmon <kkan@apcs.vn>
2021-05-21 17:12:05 +0700

    Add PLSA
    Former-commit-id: 0cdaf07f0d7860cbb73024d507fff2b5d027ace5 [formerly 3069f7a8965b95b00f4037e258e0b6a7f6f09438]
    Former-commit-id: 1742e982be164475754f1550c4e0198903d9e05c

--- pLSA/utils.py
+++ pLSA/utils.py
@@ -1,6 +1,9 @@
 def normalize(vec):
-    s = sum(vec)
-    assert (abs(s) != 0.0)  # the sum must not be 0
+
+    temp_vec = vec
+    ##temp_vec = [ele ** 0.15 for ele in temp_vec]
+    sum_temp = sum(temp_vec)
+    assert (abs(sum_temp) != 0.0)  # the sum must not be 0
     """
     if abs(s) < 1e-6:
         print "Sum of vectors sums almost to 0. Stop here."
@@ -10,4 +13,5 @@ def normalize(vec):
 
     for i in range(len(vec)):
         assert (vec[i] >= 0)  # element must be >= 0
-        vec[i] = vec[i] * 1.0 / s
+        vec[i] = (vec[i]) / (sum_temp) #0.1 is beta-distribution for TEM algo
+        #vec[i] = (vec[i]) ** 0.15 / (sum_temp) #0.1 is beta-distribution for TEM algo
