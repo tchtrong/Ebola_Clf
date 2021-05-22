ee01c18d7c5800ae5d90379d035a6a56496b3122
Tran Trong <tchtrong@apcs.vn>
2021-05-22 20:56:06 +0700

    Update processing_fasta
    Former-commit-id: 3016cfc865940dc7fe08fe1702d63ad196355c48
    Former-commit-id: 6128f74a493c27d4c63308b7789ff35cb937c8e4

--- processing_fasta.py
+++ processing_fasta.py
@@ -1,40 +1,11 @@
-#%%
+# %%
 from global_variables import organisms
 from Bio import SeqIO
 import pandas as pd
 
 
-#%%
-#Read fasta
-records = {}
-sizes = []
-for organism in organisms:
-    record = list(SeqIO.parse("fasta_original/{}.fasta".format(organism), "fasta"))
-    #Remove sequences contain X
-    record = [sequence for sequence in record if str(sequence.seq).find("X") == -1]
-    #Remove description of sequence (MEME will warn if the description is too long)
-    for sequence in record:
-        sequence.description = ""
-    #Update sizes
-    if organism == "zai":
-        sizes.append(270)
-    else:
-        sizes.append(len(record))
-    records.update({organism: record})
-
-#Save size for each species
-pd.DataFrame([sizes], columns=organisms).to_csv("fasta/sizes.csv")
-
-print(sizes)
-
-#%%
-#Save fasta
-for organism in organisms:
-    SeqIO.write(records[organism], "fasta/{}.fasta".format(organism), "fasta")
-
-#%%
-#Remove duplicate sequences
-def remove_dup_seqs(records, checksums):
+def remove_dup_seqs(records):
+    checksums = set()
     for record in records:
         checksum = record.seq
         if checksum in checksums:
@@ -42,17 +13,43 @@ def remove_dup_seqs(records, checksums):
         checksums.add(checksum)
         yield record
 
-sizes = []
-checksums = set()
+
+# %%
+# Read fasta
+sizes = {"": [], "_cleaned": []}
 for organism in organisms:
-    cleaned = remove_dup_seqs(records[organism], checksums)
+    record = list(SeqIO.parse(
+        "fasta_original/{}.fasta".format(organism), "fasta"))
+
+    # Remove sequences contain X
+    record = [sequence for sequence in record if str(
+        sequence.seq).find("X") == -1]
+
+    # Remove description of sequence (MEME will warn if the description is too long)
+    for sequence in record:
+        sequence.description = ""
+
+    # Remove duplicate sequences
+    cleaned = remove_dup_seqs(record)
+
+    # Update sizes
     if organism == "zai":
-        sizes.append(75)
-        SeqIO.write(cleaned, "fasta_cleaned/{}.fasta".format(organism), "fasta")
-    else: 
-        sizes.append(SeqIO.write(cleaned, "fasta_cleaned/{}.fasta".format(organism), "fasta"))
+        sizes[""].append(270)
+        sizes["_cleaned"].append(75)
+    else:
+        sizes[""].append(SeqIO.write(
+            record, "fasta/{}.fasta".format(organism), "fasta"))
+        sizes["_cleaned"].append(SeqIO.write(
+            cleaned, "fasta_cleaned/{}.fasta".format(organism), "fasta"))
 
-print(sizes)
+# %%
+# Save size for each species and save the labels
+for key, values in sizes.items():
+    labels = []
+    for idx, value in enumerate(values):
+        labels = labels + [idx] * value
+    pd.DataFrame(labels).to_csv("csv/labels{}.csv".format(key))
+    pd.DataFrame(sizes[key], index=organisms).to_csv(
+        "fasta{}/sizes.csv".format(key))
 
-#Save size for each species
-pd.DataFrame([sizes], columns=organisms).to_csv("fasta_cleaned/sizes.csv")
+# %%
