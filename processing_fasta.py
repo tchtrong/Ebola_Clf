661cc9c2b8595c038d1085575d061b9e934d49c2
Tran Trong <tchtrong@apcs.vn>
2021-05-23 15:25:12 +0700

    Big update
    Former-commit-id: 8ee46bb3269b58768f1361d2798647e79918e987
    Former-commit-id: 5320c8ff6aebb5495d38e9dd28d089c062fafd01

--- processing_fasta.py
+++ processing_fasta.py
@@ -33,6 +33,7 @@ for organism in organisms:
     for sequence in record:
         sequence.description = ""
         # sequence.name = idx
+        # sequence.id = idx
 
     # Remove duplicate sequences
     cleaned = remove_dup_seqs(record)
@@ -57,30 +58,3 @@ for key, values in sizes.items():
     pd.DataFrame(sizes[key], index=organisms).to_csv(
         "fasta{}/sizes.csv".format(key), index=False)
 
-# %%
-# Create all_*.fasta
-
-# Get fasta and fasta_cleaned folders
-fasta_folders = glob.glob("fasta*")
-fasta_folders = [
-    folder for folder in fasta_folders if "original" not in folder]
-
-# Create motifs folder
-p = Path('motifs')
-p.mkdir(exist_ok=True)
-
-for folder in fasta_folders:
-    not_zai = glob.glob(folder + "/[!az]*.fasta")
-    zais = glob.glob(folder + "/zai_*.fasta")
-    not_zai.sort()
-    for zai in zais:
-        seed = re.search("\d+", zai).group(0)
-        p.joinpath(seed).mkdir(exist_ok=True)
-        with open(folder + "/all_{}.fasta".format(seed), 'wb') as wfd:
-            files = not_zai.copy()
-            files.append(zai)
-            for f in files:
-                with open(f, 'rb') as fd:
-                    shutil.copyfileobj(fd, wfd)
-
-# %%
