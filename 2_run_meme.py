9a955aba9343dd22e42a8f0ae0eb7c7f17cd249c
Tran Trong <tchtrong@apcs.vn>
2021-05-27 19:47:44 +0000

    Motifs
    Former-commit-id: 37701417c317b0e5f7b0b423ea490c084e7b3faf
    Former-commit-id: 2bd5cf9695a7ca2dd26c0e0ce1c536bbf843458d

--- 2_run_meme.py
+++ 2_run_meme.py
@@ -18,17 +18,17 @@ def run_meme(no_X: bool):
     for i in range(3, 21):
         sub_folder = motifs_folder.joinpath("{}".format(i))
         sub_folder.mkdir(exist_ok=True)
-        process = subprocess.run(["meme",
+        process = subprocess.run(["/home/trongtch_vt/meme/bin/meme",
                                   "-searchsize", "0",
                                   "-protein",
                                   "-objfun", "classic",
                                   "-oc", sub_folder.as_posix(),
                                   "-mod", "zoops",
                                   "-minw", str(i), "-maxw", str(i),
-                                  "-nmotifs", "200",
+                                  "-nmotifs", "150",
                                   "-minsites", "20",
                                   "-evt", "5.0e-006",
-                                  "-p", '48 --use-hwthread-cpus',
+                                  "-p", '96 --use-hwthread-cpus',
                                   fasta.as_posix()], capture_output=True)
         if process.returncode != 0:
             print(process.stderr)
