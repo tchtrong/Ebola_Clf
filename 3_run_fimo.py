57b3532438905db225303e56cd1217d948375169
Tran Trong <tchtrong@apcs.vn>
2021-05-28 16:00:55 +0700

    Run FIMO
    Former-commit-id: 03d9529357566cb0eacf25bda8eaa76775e6d9cb
    Former-commit-id: cbaa4821273a1b31c3625669adbbd5f0c435d41d

--- 3_run_fimo.py
+++ 3_run_fimo.py
@@ -5,12 +5,8 @@ from pathlib import Path
 
 def run_fimo(path):
     p = Path(path)
-    motif_folder = Path()
-    if "X" in p.as_posix():
-        motif_folder = Path("motifs_no_X")
-    else:
-        motif_folder = Path("motifs")
-    list_fasta = list(p.glob("[!a]??.fasta"))
+    motif_folder = Path("motifs_no_X")
+    list_fasta = list(p.glob("[!a]??[!_]fasta"))
     fimo_dir = Path('fimo' + p.as_posix()[5:])
     fimo_dir.mkdir(exist_ok=True)
     for fasta in list_fasta:
@@ -19,16 +15,20 @@ def run_fimo(path):
         species_dir.mkdir(exist_ok=True)
         for i in range(3, 21):
             file_motif = motif_folder.joinpath(
-                "{}_{}".format(i, i)).joinpath("meme.txt")
+                "{}".format(i)).joinpath("meme.txt")
             out = subprocess.run(["fimo",
                                   "--skip-matched-sequence",
                                   "--max-stored-scores", "1000000",
                                   file_motif.as_posix(),
                                   fasta.as_posix()],
                                  capture_output=True)
-            with open(species_dir / "{}.txt".format(i), "wb") as f:
-                f.write(out.stdout)
+            if out.returncode == 1:
+                print(out.stderr)
+            else:
+                with open(species_dir / "{}.txt".format(i), "wb") as f:
+                    f.write(out.stdout)
 
 
 # %%
 run_fimo("fasta_no_X")
+run_fimo("fasta_cleaned_no_X")
