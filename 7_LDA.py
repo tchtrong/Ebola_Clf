725ff8dc57286d765ad4b66015da36ce1a338c71
Tran Trong <tchtrong@apcs.vn>
2021-05-30 03:58:00 +0700

    Update
    Former-commit-id: 7c64bf4c6482a6609d97b357fd3fcacea2c449b1
    Former-commit-id: b59946314b11385f5b9cf49685886e5513ec3693

--- 7_LDA.py
+++ 7_LDA.py
@@ -1,8 +1,7 @@
 # %%
 from sklearn.decomposition import LatentDirichletAllocation as lda
 from utils.spliting_train_test import get_train_test_set
-from sklearn import svm
-from joblib import dump, load
+from joblib import dump
 import pandas as pd
 
 
@@ -15,5 +14,5 @@ for i in range(10, 2540, 10):
     lda_model = lda(n_components=i, random_state=43, n_jobs=-1)
     lda_model.fit(X_train)
     dump(lda_model, 'LDA_models/LDA_{}_train_only'.format(i))
-    lda_model.fit(pd.concat[X_train, X_test])
+    lda_model.fit(pd.concat([X_train, X_test]))
     dump(lda_model, 'LDA_models/LDA_{}_train_test'.format(i))
