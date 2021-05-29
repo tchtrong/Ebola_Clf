39c6a1c73c8ca4aa885b46d4e5984aa6b117c85f
Tran Trong <tchtrong@apcs.vn>
2021-05-30 03:54:03 +0700

    Add LDA
    Former-commit-id: 5a522c4cb044c567a5a85807bff96c39cb1c0d9a
    Former-commit-id: e7b741ae73ae52dcb99e864e3bf8584c26f63fe2

--- 7_LDA.py
+++ 7_LDA.py
@@ -2,6 +2,8 @@
 from sklearn.decomposition import LatentDirichletAllocation as lda
 from utils.spliting_train_test import get_train_test_set
 from sklearn import svm
+from joblib import dump, load
+import pandas as pd
 
 
 # %%
@@ -9,15 +11,9 @@ X_train, X_test, y_train, y_test = get_train_test_set(
     is_fimo=True, is_cleaned=True)
 
 # %%
-score_max = [0, 0]
 for i in range(10, 2540, 10):
-    lda_model = lda(n_components=i, random_state=43, n_jobs=4)
+    lda_model = lda(n_components=i, random_state=43, n_jobs=-1)
     lda_model.fit(X_train)
-    X_train_LDA = lda_model.transform(X_train)
-    X_test_LDA = lda_model.transform(X_test)
-    clf = svm.SVC()
-    clf.fit(X_train_LDA, y_train.values.ravel())
-    score = clf.score(X_test_LDA, y_test.values.ravel())
-    if score > score_max[0]:
-        score_max[0] = score
-        score_max[1] = i
+    dump(lda_model, 'LDA_models/LDA_{}_train_only'.format(i))
+    lda_model.fit(pd.concat[X_train, X_test])
+    dump(lda_model, 'LDA_models/LDA_{}_train_test'.format(i))
---
+++ 7_LDA_2.py
@@ -0,0 +1,24 @@
+# %%
+from sklearn.decomposition import LatentDirichletAllocation as lda
+from utils.spliting_train_test import get_train_test_set
+from sklearn import svm
+import pandas as pd
+
+
+# %%
+X_train, X_test, y_train, y_test = get_train_test_set(
+    is_fimo=True, is_cleaned=True)
+
+# %%
+score_max = [0, 0]
+for i in range(10, 2540, 10):
+    lda_model = lda(n_components=i, random_state=43, n_jobs=4)
+    lda_model.fit(pd.concat([X_train,X_test]))
+    X_train_LDA = lda_model.transform(X_train)
+    X_test_LDA = lda_model.transform(X_test)
+    clf = svm.SVC()
+    clf.fit(X_train_LDA, y_train.values.ravel())
+    score = clf.score(X_test_LDA, y_test.values.ravel())
+    if score > score_max[0]:
+        score_max[0] = score
+        score_max[1] = i
