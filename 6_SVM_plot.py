a5fb759773a17ade40596399b6121cf85f6ca249
Tran Trong <tchtrong@apcs.vn>
2021-05-30 17:51:27 +0700

    Update results
    Former-commit-id: ca651ffb048cff027a6e176b03aa2c714a4039ca
    Former-commit-id: 3f2f3455534f78b7fcffbdf6729b11182e7e0a25

--- 6_SVM_plot.py
+++ 6_SVM_plot.py
@@ -1,9 +1,6 @@
 # %%
 from sklearn import svm
 from utils.spliting_train_test import get_train_test_set
-from pathlib import Path
-import pandas as pd
-from utils.utils import get_file_name
 import matplotlib.pyplot as plt
 
 # %%
@@ -16,84 +13,86 @@ Cs = [0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
 
 # %%
 
-plt.figure(figsize=(10, 20))
+plt.figure(figsize=(10, 5))
 
-results1 = []
+results = []
 for Cs_ in Cs:
     clf = svm.SVC(kernel='linear', C=Cs_)
     clf.fit(X_train, y_train.values.ravel())
-    results1.append(clf.score(X_test, y_test.values.ravel()))
+    results.append(clf.score(X_test, y_test.values.ravel()))
 
-plt.subplot(321)
-plt.plot(Cs, results1)
+plt.subplot(231)
+plt.plot(Cs, results)
 plt.xlabel('C')
 plt.ylabel('Accuracy')
 plt.xscale('log')
 plt.title('Kernel: Linear')
 
-results2 = []
+results = []
 for Cs_ in Cs:
     clf = svm.SVC(C=Cs_)
     clf.fit(X_train, y_train.values.ravel())
-    results2.append(clf.score(X_test, y_test.values.ravel()))
+    results.append(clf.score(X_test, y_test.values.ravel()))
 
-plt.subplot(322)
-plt.plot(Cs, results2)
+plt.subplot(232)
+plt.plot(Cs, results)
 plt.xlabel('C')
 plt.ylabel('Accuracy')
 plt.title('Kernel: RBF, Gamma: Auto')
 plt.xscale('log')
 
-results3 = []
+results = []
 for gamma_ in gammas:
     clf = svm.SVC(gamma=gamma_)
     clf.fit(X_train, y_train.values.ravel())
-    results3.append(clf.score(X_test, y_test.values.ravel()))
+    results.append(clf.score(X_test, y_test.values.ravel()))
 
-plt.subplot(323)
-plt.plot(gammas, results3)
+plt.subplot(233)
+plt.plot(gammas, results)
 plt.xlabel('Gamma')
 plt.ylabel('Accuracy')
 plt.title('Kernel: RBF, C: 1.0')
 
-results4 = []
+results = []
 for gamma_ in gammas:
-    clf = svm.SVC(gamma=gamma_, C=10.0)
+    clf = svm.SVC(gamma=gamma_, C=100.0)
     clf.fit(X_train, y_train.values.ravel())
-    results4.append(clf.score(X_test, y_test.values.ravel()))
+    results.append(clf.score(X_test, y_test.values.ravel()))
 
-plt.subplot(324)
-plt.plot(gammas, results4)
+plt.subplot(234)
+plt.plot(gammas, results)
 plt.xlabel('Gamma')
 plt.ylabel('Accuracy')
-plt.title('Kernel: RBF, C: 10.0')
+plt.title('Kernel: RBF, C: 100.0')
 
-results4 = []
+results = []
 for gamma_ in gammas:
     clf = svm.SVC(gamma=gamma_, kernel='poly')
     clf.fit(X_train, y_train.values.ravel())
-    results4.append(clf.score(X_test, y_test.values.ravel()))
+    results.append(clf.score(X_test, y_test.values.ravel()))
 
-plt.subplot(325)
-plt.plot(gammas, results4)
+plt.subplot(235)
+plt.plot(gammas, results)
 plt.xlabel('Gamma')
 plt.ylabel('Accuracy')
 plt.title('Kernel: Poly, C: 1.0')
 
-results4 = []
+results = []
 for gamma_ in gammas:
     clf = svm.SVC(gamma=gamma_, C=10.0, kernel='poly')
     clf.fit(X_train, y_train.values.ravel())
-    results4.append(clf.score(X_test, y_test.values.ravel()))
+    results.append(clf.score(X_test, y_test.values.ravel()))
 
-plt.subplot(326)
-plt.plot(gammas, results4)
+plt.subplot(236)
+plt.plot(gammas, results)
 plt.xlabel('Gamma')
 plt.ylabel('Accuracy')
 plt.title('Kernel: Poly, C: 10.0')
 
 
-plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.5,
                     wspace=0.35)
+
+plt.savefig('SVM.svg')
 plt.show()
 # %%
---
+++ 7_LDA_SVM.py
@@ -0,0 +1,45 @@
+# %%
+from sklearn.decomposition import LatentDirichletAllocation as lda
+from utils.spliting_train_test import get_train_test_set
+from sklearn import svm
+from joblib import load
+from matplotlib import pyplot as plt
+
+# %%
+X_train, X_test, y_train, y_test = get_train_test_set(
+    is_fimo=True, is_cleaned=True)
+
+results_linear = []
+results_rbf = []
+for i in range(10, 2540, 100):
+    lda_model = load('LDA_models/LDA_{}_train_test'.format(i))
+    X_LDA_train = lda_model.transform(X_train)
+    X_LDA_test = lda_model.transform(X_test)
+    clf_linear = svm.SVC(kernel='linear')
+    clf_rbf = svm.SVC()
+    clf_linear.fit(X_LDA_train, y_train.values.ravel())
+    clf_rbf.fit(X_LDA_train, y_train.values.ravel())
+    results_linear.append(clf_linear.score(X_LDA_test, y_test.values.ravel()))
+    results_rbf.append(clf_rbf.score(X_LDA_test, y_test.values.ravel()))
+# %%
+plt.figure(figsize=(10, 5))
+
+plt.subplot(121)
+plt.plot(list(range(10, 2540, 100)), results_linear)
+plt.xlabel('Dimensions')
+plt.ylabel('Accuracy')
+plt.title('LDA on train + test, SVM with linear kernel')
+
+plt.subplot(122)
+plt.plot(list(range(10, 2540, 100)), results_rbf)
+plt.xlabel('Dimensions')
+plt.ylabel('Accuracy')
+plt.title('LDA on train + test, SVM with RBF kernel')
+
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                    wspace=0.35)
+plt.savefig('LDA_train_test.svg')
+plt.show()
+
+
+# %%
