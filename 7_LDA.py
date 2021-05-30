31e9cfe51628fdc9746874c31a6981856b710564
Tran Trong <tchtrong@apcs.vn>
2021-05-30 04:26:38 +0000

    LDA models
    Former-commit-id: 27c354e0adff336c72b86bbb5729554055267e24
    Former-commit-id: 35283220273f31d5e631ddbbb051dca5103ffce9

--- 7_LDA.py
+++ 7_LDA.py
@@ -3,6 +3,7 @@ from sklearn.decomposition import LatentDirichletAllocation as lda
 from utils.spliting_train_test import get_train_test_set
 from joblib import dump
 import pandas as pd
+import gc
 
 
 # %%
@@ -10,9 +11,14 @@ X_train, X_test, y_train, y_test = get_train_test_set(
     is_fimo=True, is_cleaned=True)
 
 # %%
-for i in range(10, 2540, 10):
+for i in range(1910, 2540, 100):
     lda_model = lda(n_components=i, random_state=43, n_jobs=-1)
     lda_model.fit(X_train)
     dump(lda_model, 'LDA_models/LDA_{}_train_only'.format(i))
+    del lda_model
+    gc.collect()
+    lda_model = lda(n_components=i, random_state=43, n_jobs=-1)
     lda_model.fit(pd.concat([X_train, X_test]))
     dump(lda_model, 'LDA_models/LDA_{}_train_test'.format(i))
+    del lda_model
+    gc.collect()
