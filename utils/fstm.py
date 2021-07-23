aad13806669eb69c03b6249891b867a35d421696
Tran Trong <tchtrong@apcs.vn>
2021-07-24 02:26:13 +0700

    FSTM

--- utils/fstm.py
+++ utils/fstm.py
@@ -7,6 +7,7 @@ from sklearn.decomposition import LatentDirichletAllocation, TruncatedSVD
 from sklearn.preprocessing import normalize
 from sklearn.utils.validation import check_random_state
 from scipy.optimize import minimize_scalar
+from utils.fast_fstm import e_step
 
 EPS = np.finfo(float).eps
 
@@ -65,6 +66,8 @@ class FSTM(BaseEstimator, TransformerMixin):
         # This is theta in literature
         doc_topic_distr = np.zeros(
             shape=(n_samples, n_components), dtype=np.float64)
+        ncv = np.zeros(n_components, dtype=np.float64)
+        nfv = np.zeros(n_samples, dtype=np.float64)
 
         converge = 1.0
         likelihood_old = 1.0
@@ -89,7 +92,7 @@ class FSTM(BaseEstimator, TransformerMixin):
                 # the problem to 2 cases
                 if em_iter > 0 and self.warm_start:
                     fx: np.ndarray = self._f(
-                        X[ind_d], doc_topic_distr[ind_d].dot(self.components_))
+                        X[ind_d], doc_topic_distr[ind_d][:, np.newaxis] * (self.components_))
                     r = fx.argmax()
                 else:
                     # Calculate f(x) = Sigma(d_j * log(x_j))
@@ -97,10 +100,13 @@ class FSTM(BaseEstimator, TransformerMixin):
                     r = fx.argmax()
                     doc_topic_distr[ind_d][r] = 1
 
-                likelihood += self._e_step(X[ind_d],
-                                           self.components_, r, doc_topic_distr[ind_d])
+                likelihood += e_step(self.inf_max_iter, self.inf_converge, self.alpha_max_iter, X[ind_d],
+                                     self.components_, self.components_[
+                                         r].copy(), doc_topic_distr[ind_d],
+                                     ncv, nfv)
 
             # M-step
+            normalize(doc_topic_distr, norm='l1', copy=False, axis=0)
             self.components_ = doc_topic_distr.transpose().dot(X)
             normalize(self.components_, norm='l1', copy=False)
 
@@ -117,82 +123,10 @@ class FSTM(BaseEstimator, TransformerMixin):
         return doc_topic_distr
 
     def _f(self, document: np.ndarray, x: np.ndarray) -> np.ndarray:
-        """Calculate f(x) in literature: f(x) = sum_{j}(d_j * log(x_j)) = d * log(x).T
-
-        Parameters
-        ----------
-        document : 1 x n_features matrix
-
-        x : {sparse matrix} n_components x n_features
-
-        Returns
-        -------
-        1 x n_components matrix
-        """
-
         log_x = x.copy()
         nz = log_x != 0
         log_x[nz] = np.log(log_x[nz])
-
         return document.dot(log_x.transpose())
-
-    def _grad_f(self, document: np.ndarray, x: np.ndarray) -> np.ndarray:
-        """Calculate gradient of f(x) in literature
-
-        Parameters
-        ----------
-        document : 1 x n_features matrix
-
-        x : {sparse matrix} n_components x n_features
-
-        Returns
-        -------
-        1 x n_components matrix
-        """
-
-        x_ = x.copy()
-        nz = x_ != 0
-        x_[nz] = 1 / x_[nz]
-
-        return np.multiply(document, x_)
-
-    def _f_alpha(self, alpha: float, document: np.ndarray, x: np.ndarray, beta_i: np.ndarray) -> np.float64:
-        """Calculate f(x) in literature: f(x) = sum_{j}(d_j * log(x_j)) = d * log(x).T
-
-        Parameters
-        ----------
-        document : 1 x n_features matrix
-
-        x : {sparse matrix} n_components x n_features
-
-        Returns
-        -------
-        1 x n_components matrix or 1 x 1 matrix (scaler)
-        """
-
-        new_x = x + (beta_i - x) * alpha
-
-        return (self._f(document, new_x) * -1)
-
-    def _e_step(self, document: np.ndarray, beta: np.ndarray, x_ind: int, theta: np.ndarray) -> float:
-        x = beta[x_ind]
-        likelihood_old = 0
-        converge = 0
-        for inf_iter in range(self.inf_max_iter):
-            grad = self._grad_f(document, x)
-            i = grad.dot(beta.transpose()).argmax()
-            alpha = minimize_scalar(
-                self._f_alpha, bounds=(0, 1), args=(document, x, beta[i]), method='bounded', options={'maxiter': self.alpha_max_iter})
-            x = alpha.x * beta[i] + (1 - alpha.x) * x
-            theta *= (1 - alpha.x)
-            theta[i] += alpha.x
-            likelihood = self._f(document, x)
-            if inf_iter > 0:
-                converge = (likelihood_old - likelihood)/likelihood_old
-            likelihood_old = likelihood
-            if converge > 0 and converge < self.inf_converge:
-                break
-        return likelihood_old
 
     def transform(self, X, y=None):
         X: np.ndarray = self._validate_data(X, ensure_min_features=2)
@@ -202,6 +136,8 @@ class FSTM(BaseEstimator, TransformerMixin):
 
         doc_topic_distr = np.zeros(
             shape=(n_samples, n_components), dtype=np.float64)
+        ncv = np.zeros(n_components, dtype=np.float64)
+        nfv = np.zeros(n_samples, dtype=np.float64)
 
         log_x_j = self.components_.copy()
         nz = log_x_j != 0
@@ -212,7 +148,10 @@ class FSTM(BaseEstimator, TransformerMixin):
             r = fx.argmax()
             doc_topic_distr[ind_d][r] = 1
 
-            self._e_step(X[ind_d],
-                         self.components_, r, doc_topic_distr[ind_d])
+            e_step(self.inf_max_iter, self.inf_converge, self.alpha_max_iter, X[ind_d],
+                   self.components_, self.components_[
+                r].copy(), doc_topic_distr[ind_d],
+                ncv, nfv)
 
+        normalize(doc_topic_distr, norm='l1', copy=False, axis=0)
         return doc_topic_distr
